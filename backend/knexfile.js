22a74adf1ba23d04fb011e6c3bd4dd6445401ba0
mabrar2011 <mabrar2011@gmail.com>
2026-03-31 16:18:02 +0000

    feat: add knexfile for migrations

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,3 +1,13 @@
+'use strict';
+require('dotenv').config();
 const { baseConfig } = require('./config/database');
 
-module.exports = baseConfig;
+module.exports = {
+  development: {
+    ...baseConfig,
+  },
+
+  production: {
+    ...baseConfig,
+  },
+};
